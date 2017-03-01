1a323c021ba623fdd3ba4b6528f09e2589f55840
learneasy2016 <learneasy2016@github.com>
2017-02-28 22:45:35 -0500

    cleanup + adjust widget position

--- app/components/network-editor/defs/layer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class LayerType {
-}
-exports.LayerType = LayerType;
-class Layer {
-}
-exports.Layer = Layer;
-//# sourceMappingURL=layer.js.map
--- app/components/network-editor/defs/link.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Record the links betwen layers.
-class Link {
-}
-exports.Link = Link;
-//# sourceMappingURL=link.js.map
