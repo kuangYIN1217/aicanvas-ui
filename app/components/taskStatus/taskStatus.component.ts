5bb5787c4f60107f19424e737dbc8db9a5408b7b
BlandLi <ligang7560@gmail.com>
2017-05-02 16:27:49 +0800

    fix 进度小数点后两位的问题

--- app/components/taskStatus/taskStatus.component.ts
+++ app/components/taskStatus/taskStatus.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
-import { Location } from '@angular/common'
-import { ResourcesService } from '../../common/services/resources.service'
+import {Location} from '@angular/common'
+import {ResourcesService} from '../../common/services/resources.service'
 import {modelService} from "../../common/services/model.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {PluginService} from "../../common/services/plugin.service";
@@ -12,97 +12,111 @@ import {bindDirectiveInputs} from "@angular/compiler/src/view_compiler/property_
     selector: 'taskStatus',
     styleUrls: ['./css/taskStatus.component.css'],
     templateUrl: './templates/taskStatus.html',
-    providers: [ResourcesService,modelService,PluginService,JobService]
+    providers: [ResourcesService, modelService, PluginService, JobService]
 })
-export class TaskStatusComponent{
+export class TaskStatusComponent {
     page: number = 1;
     pageMaxItem: number = 10;
     interval: any;
     Jobs: JobInfo[] = [];
     Jobs_current: JobInfo[] = [];
     createdJob: JobInfo = new JobInfo();
-    @Input() statuss:string;
+    @Input() statuss: string;
 
-    constructor(private  modelService:modelService,private jobService: JobService, private location: Location, private route: ActivatedRoute ,private router: Router){
-        //this.interval = setInterval (() => {this.updatePage()}, 500);
+    constructor(private  modelService: modelService, private jobService: JobService, private location: Location, private route: ActivatedRoute, private router: Router) {
 
     }
-   ngOnInit(){
-       this.updatePage();
+
+    ngOnInit() {
+        this.interval = setInterval(() => {
+            this.updatePage()
+        }, 500);
     }
-    updatePage(){
-            this.getAlljobs(this.statuss,this.page-1,this.pageMaxItem);
+
+    updatePage() {
+        this.getAlljobs(this.page - 1, this.pageMaxItem);
     }
-    getAlljobs(page,size){
-        this.jobService.getAllJobs(page,size)
+
+    getAlljobs(page, size) {
+        this.jobService.getAllJobs(this.statuss, page, size)
             .subscribe(Jobs => {
                 this.Jobs = Jobs.content;
                 this.Jobs_current = Jobs.content;
                 this.createdJob = Jobs;
             });
     }
-    ngOnDestroy(){
+
+    ngOnDestroy() {
         // 退出时停止更新
         clearInterval(this.interval);
     }
-    checkStatus(status,sence , jobPath){
-        if(status=='Finished'){
-            this.modelService.getStatue(jobPath).subscribe(data=>{
-                this.router.navigate(['../model'],{queryParams: { sence: sence }});
+
+    checkStatus(status, sence, jobPath) {
+        if (status == 'Finished') {
+            this.modelService.getStatue(jobPath).subscribe(data => {
+                this.router.navigate(['../model'], {queryParams: {sence: sence}});
             });
             //TODO if success give alert
 
-        }else{
+        } else {
             return false;
         }
     }
-    start(jobPath: string){
+
+    start(jobPath: string) {
         this.jobService.runJob(jobPath)
             .subscribe(reply => this.start_reply(reply));
     }
-    start_reply(reply){
-        if(reply.status==200){
+
+    start_reply(reply) {
+        if (reply.status == 200) {
             console.log("Start Successfully!");
-        }else{
+        } else {
             console.log("Start Failed!");
         }
         this.updatePage();
     }
-    stop(jobPath: string){
+
+    stop(jobPath: string) {
         this.jobService.stopJob(jobPath)
             .subscribe(reply => this.stop_reply(reply));
     }
-    stop_reply(reply){
-        if(reply.status==200){
+
+    stop_reply(reply) {
+        if (reply.status == 200) {
             console.log("Stoped!");
-        }else{
+        } else {
             console.log("Stop Failed!");
         }
         this.updatePage();
     }
-    maxItemChange(){
-        this.page=1;
-        this.getAlljobs(this.statuss,this.page-1,this.pageMaxItem);
+
+    maxItemChange() {
+        this.page = 1;
+        this.getAlljobs(this.page - 1, this.pageMaxItem);
         console.log(this.createdJob);
     }
-    nextPage(){
+
+    nextPage() {
         this.page++;
-        this.getAlljobs(this.statuss,this.page-1,this.pageMaxItem);
+        this.getAlljobs(this.page - 1, this.pageMaxItem);
         console.log(this.createdJob);
     }
-    previousPage(){
-        if (this.page>1){
+
+    previousPage() {
+        if (this.page > 1) {
             this.page--;
-            this.getAlljobs(this.statuss,this.page-1,this.pageMaxItem);
-        }else{
+            this.getAlljobs(this.page - 1, this.pageMaxItem);
+        } else {
             alert('已经是首页');
         }
     }
-    output(percent){
-        if(percent==100){
-            return parseInt(percent)+"%";
-        }else{
-            return percent.toFixed(2)+"%";
+
+    output(percent) {
+        if (percent == 100) {
+            return parseInt(percent) + "%";
+        } else {
+            return percent.toFixed(2) + "%";
         }
     }
 }
