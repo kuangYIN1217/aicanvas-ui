0fadb8add2625aedd6111905bc9be2a0655d9040
BlandLi <ligang7560@gmail.com>
2017-05-10 11:05:48 +0800

    删除远程仓库

--- app/main.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AicanvasModule } from './aicanvas.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AicanvasModule);
