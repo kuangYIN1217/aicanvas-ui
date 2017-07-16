00840fc1f237f849cb72b138dd4007f803bd3ea4
hinesboy <hines.zhu@gmail.com>
2017-07-16 13:06:26 +0800

    datasets

--- src/app/datasets/datasets.component.ts
+++ src/app/datasets/datasets.component.ts
@@ -1,10 +1,104 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {DatasetsService} from "../common/services/datasets.service";
+import {Page} from "../common/defs/resources";
 @Component({
   selector: 'datasets',
-  templateUrl: './templates/datasets.html'
+  styleUrls: ['./datasets.component.css'],
+  templateUrl: './datasets.html',
+  providers: [DatasetsService]
 })
-export class DatasetsComponent implements OnInit{
-  ngOnInit(): void {
+export class DatasetsComponent{
+  username: string = '';
+  s_popup_show: boolean = false;
+  d_dataTypes: any = [];
+  d_tableData: any = [];
+  // 筛选条件
+  s_nav_selected: number = 0; // create
+  s_select_datasetType: any = 'all'; // type
+  s_select_name: any = ''; // name
+  s_sort_type: any = 'createTime,desc';
+  s_page: any = 1;
+  s_size: any = 10;
+
+  pageParams=new Page();
+  constructor (private datasetservice: DatasetsService) {
+    this.pageParams.pageMaxItem = this.s_size;
+    this.pageParams.curPage = this.s_page;
+    // 获取datasetType
+    this.getDataSetsTypes();
+    this.username = sessionStorage['username'];
+    this.initTable();
+  }
+
+  // -----初始化数据 ------------------------------------------------
+  getDataSetsTypes() {
+    this.datasetservice.getDataSetType().subscribe(rep => {
+      this.d_dataTypes = rep;
+      console.log(this.d_dataTypes)
+    })
+  }
+  initTable () {
+    let creator , dataSetType , name , sort, page , size;
+    creator = this.s_nav_selected ;
+    dataSetType = this.s_select_datasetType ;
+    name = this.s_select_name ;
+    sort = this.s_sort_type;
+    page = this.s_page ;
+    size = this.s_size;
+    creator = creator === 1 ? 'system' : this.username;
+    dataSetType = dataSetType === 'all' ? null : dataSetType;
+    if (!name) {
+      name = null;
+    }
+    if (sort) {
+      name = null;
+    }
+    this.datasetservice.getDataSets(creator , dataSetType , name , sort, page , size ).subscribe(rep =>{
+      this.d_tableData = rep.content;
+      console.log(this.d_tableData)
+      this.changePageParmas(rep);
+    })
+  }
+  // -----初始化数据 end --------------------------------------------
+
+  $nav_click(index) {
+    this.s_select_datasetType = 'all'; // type
+    this.s_nav_selected = index;
+    this.initTable();
+  }
+  $upload_click () {
+    if (!this.s_popup_show) {
+      this.s_popup_show = true;
+    }
+  }
+
+  $select_change() {
+    console.log(this.s_select_datasetType)
+    this.initTable ();
+  }
+
+  $name_change () {
+    console.log(this.s_select_name);
+    this.initTable();
+  }
+
+  changePageParmas(result) {
+    console.log(result)
+    let page = new Page();
+    page.pageMaxItem = result.size;
+    page.curPage = result.number+1;
+    page.totalPage = result.totalPages;
+    page.totalNum = result.totalElements;
+    this.s_page = page.curPage;
+    this.s_size = page.pageMaxItem;
+    this.pageParams = page;
+  }
+
+  getPageData(paraParam) {
+    this.s_page = paraParam.curPage;
+    this.s_size = paraParam.pageMaxItem
+    this.initTable();
+    //console.log('触发', paraParam);
   }
 }
 
---
+++ src/app/datasets/mydata/mydata.component.ts
@@ -0,0 +1,57 @@
+/**
+ * Created by Administrator on 2017/7/12 0012.
+ */
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {DatasetsService} from "../../common/services/datasets.service";
+import {calc_size} from '../calc-size'
+@Component({
+  selector: 'cpt-mydata',
+  styleUrls: ['./mydata.component.css'],
+  templateUrl: './mydata.component.html',
+  providers: [DatasetsService]
+})
+export class MyDataComponent{
+  @Input() s_sort_type: any;
+  @Output() s_sort_typeChange: EventEmitter<any> = new EventEmitter();
+  @Input() d_tableData: any = [];
+  @Output() d_tableDataChange: EventEmitter<any> = new EventEmitter();
+
+  @Input() dataSetType: any;
+  @Input() s_select_datasetType: any = 'all'; // type
+  @Input() s_select_name: any = ''; // name
+  @Input() s_page: any;
+  @Input() s_size: any;
+  constructor (private datasetservice: DatasetsService) {
+    // 获取datasetType
+    /*this.datasetservice.getDataSets(null , null , null , null, 1 , 10 ).subscribe(rep =>{
+      console.log(rep);
+    })*/
+  }
+
+  $remove_dataSet(id , index) {
+    this.datasetservice.deleteDataSet(id).subscribe(rep => {
+      if (rep.ok) {
+        this.d_tableData.splice(index , 1);
+        this.d_tableDataChange.emit(this.d_tableData);
+      }
+    })
+  }
+
+  $sort_click () {
+    console.log('click sort')
+    if (this.s_sort_type === 'createTime,asc') {
+      this.s_sort_type = 'createTime,desc'
+    } else {
+      this.s_sort_type = 'createTime,asc'
+    }
+    this.s_sort_typeChange.emit(this.s_sort_type);
+    // 重新获取数据
+    this.datasetservice.getDataSets(sessionStorage['username'] , this.dataSetType , this.s_select_name , this.s_sort_type, this.s_page , this.s_size ).subscribe(rep =>{
+      this.d_tableData = rep.content;
+      console.log('reload data')
+      console.log(this.d_tableData)
+      this.d_tableDataChange.emit(this.d_tableData);
+    })
+  }
+  calc_size = calc_size;
+}
