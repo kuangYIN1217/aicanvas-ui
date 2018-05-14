4ef26ba8422d9f09ddf2faf9d24d568c0528aba8
zhaoyan-xinktech <was131921>
2018-05-14 14:55:21 +0800

    add training log download zip file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sed -i "s/&REPLACE_HOST&:&REPLACE_PORT_DT&/$HOST_IP:$HOST_PORT_DT/g" ./main.bundle.js
-sed -i "s/&REPLACE_HOST&:&REPLACE_PORT_DATASET&/$HOST_IP:$HOST_PORT_DATASET/g" ./main.bundle.js
+sed -i "s/&REPLACE_HOST_DT&:&REPLACE_PORT_DT&/$HOST_IP_DT:$HOST_PORT_DT/g" ./main.bundle.js;
+sed -i "s/&REPLACE_HOST_DS&:&REPLACE_PORT_DS&/$HOST_IP_DS:$HOST_PORT_DS/g" ./main.bundle.js;
 nginx -g "daemon off;"
